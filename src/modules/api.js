3b172ed208e625753f9f554e184e818227995b54
Tobi Oyero <tobioyero@gmail.com>
2024-04-17 20:15:27 -0400

    added api fetch requests

--- src/modules/api.js
+++ src/modules/api.js
@@ -4,16 +4,33 @@ const api = (() => {
 
     const apiKey = 'ba0f81137d714181bdf171340241104';
 
+    // api call to get 'current' data
     const getCurrentData = async (city) => {
         try {
             const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`, {mode: 'cors'});
-            const weatherData = await response.json();
+            const currentData = await response.json();
+            return currentData;
         } catch (e) {
             console.log(e);
         }
     }
 
-    const getForecastData = (city) => {
-        
+    //api call to get forecast data
+    const getForecastData = async (city) => {
+        try {
+            const response = await fetch (`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}`, {mode: 'cors'});
+            const forecastData = await response.json();
+            return forecastData;
+        } catch (e) {
+            console.log(e)
+        }
     }
+
+    return {
+        getCurrentData,
+        getForecastData
+    }
+
 })()
+
+export default api;
