5b01c4d376efce1ec52ce1a2cd11de887d927542
Tobi Oyero <tobioyero@Tobis-MacBook-Pro-2.local>
2024-04-24 21:41:53 -0400

    added precipitation

--- src/modules/view.js
+++ src/modules/view.js
@@ -17,66 +17,75 @@ const elementMaker = (el, id, type, txt, ph) => {
 
 const view = (() => {
     //top view -> input search bar | search btn |  celsius btn | fahrenheit btn
-    const topView = elementMaker(div, 'top-view');
+    const topView = elementMaker('div', 'top-view');
 
-    const searchBox = elementMaker(div, 'search-box');
-    const searchInput = elementMaker(input, 'search-input', 'text', '', 'search for a location');
-    const searchBtn = elementMaker(button, 'search-button', 'submit', 'Search');
+    const searchBox = elementMaker('div', 'search-box');
+    const searchInput = elementMaker('input', 'search-input', 'text', '', 'search for a location');
+    const searchBtn = elementMaker('button', 'search-button', 'submit', 'Search');
     appendChildren(searchBox, [searchInput, searchBtn]);
 
-    const sysBox = elementMaker(div, 'system-settings');
-    const celsiusBtn = elementMaker(button, 'celsius-button', txt='Celsius');
-    const fahrenheitBtn = elementMaker(button, 'fahrenheit-button', txt='Fahrenheit');
+    const sysBox = elementMaker('div', 'system-settings');
+    const celsiusBtn = elementMaker('button', 'celsius-button', txt='Celsius');
+    const fahrenheitBtn = elementMaker('button', 'fahrenheit-button', txt='Fahrenheit');
     appendChildren(sysBox, [celsiusBtn, fahrenheitBtn]);
     appendChildren(topView, [searchBox, sysBox]);
 
     //middle view -> location, time, date
-    const midView = elementMaker(div, 'mid-view');
-    const locationTxt = elementMaker(h4, 'location-text', '', '');
-    const descTxt = elementMaker(div, 'date-time');
-    const timeTxt = elementMaker(p, 'time-text','','');
-    const dateTxt = elementMaker(p, 'date-text','','');
+    const midView = elementMaker('div', 'mid-view');
+    const locationTxt = elementMaker('h4', 'location-text', '', '');
+    const descTxt = elementMaker('div', 'date-time');
+    const timeTxt = elementMaker('p', 'time-text','','');
+    const dateTxt = elementMaker('p', 'date-text','','');
     appendChildren(descTxt, [timeTxt, dateTxt]);
     appendChildren(midView, [locationTxt, descTxt]);
     
 
     //info view -> temp, feels like temp, high/low | forecast (for the day)
     //             pressure, humidity, visibility | cloudiness, wind speed, wind direction
-    const infoView = elementMaker(div, 'info-view');
+    const infoView = elementMaker('div', 'info-view');
 
-    const tempView = elementMaker(div, 'tempView'); //top left
+    const tempView = elementMaker('div', 'tempView'); //top left
 
-    const tempTxt = elementMaker(div, 'temp-text');
-    const actualTemp = elementMaker(p, 'actual-temp');
-    const feelsLikeTemp = elementMaker(p, 'feels-like-temp');
+    const tempTxt = elementMaker('div', 'temp-text');
+    const actualTemp = elementMaker('p', 'actual-temp');
+    const feelsLikeTemp = elementMaker('p', 'feels-like-temp');
     appendChildren(tempTxt, [actualTemp, feelsLikeTemp]);
 
-    const tempDesc = elementMaker(div, 'temp-desc');
-    const tempIcon = elementMaker(img);
-    const tempSumm = elementMaker(p, 'temp-summary');
+    const tempDesc = elementMaker('div', 'temp-desc');
+    const tempIcon = elementMaker('img');
+    const tempSumm = elementMaker('p', 'temp-summary');
     appendChildren(tempDesc, [tempIcon, tempSumm]);
 
-    const highLow = elementMaker(div, 'high-low');
-    const todayHigh = elementMaker(p, 'todays-high');
-    const todayLow = elementMaker(p, 'todayLow');
+    const highLow = elementMaker('div', 'high-low');
+    const todayHigh = elementMaker('p', 'todays-high');
+    const todayLow = elementMaker('p', 'todayLow');
     appendChildren(highLow, [todayHigh, todayLow]);
 
     appendChildren(tempView, [tempTxt, tempDesc, highLow]);
 
-    const forecastView = elementMaker(div, 'forecast-view'); //top right -> to fill with forecast components later
+    const forecastView = elementMaker('div', 'forecast-view'); //top right -> to fill with forecast components later
 
-    const waterView = elementMaker(div, 'water-view'); //bottom left
-    const pressureTxt = elementMaker(p, 'pressure-text');
-    const humidityTxt = elementMaker(p, 'humidity-text');
-    const visibilityTxt = elementMaker(p, 'visibility-text');
-    appendChildren(waterView, [pressureTxt, humidityTxt, visibilityTxt]);
+    const waterView = elementMaker('div', 'water-view'); //bottom left
+    const pressureTxt = elementMaker('p', 'pressure-text');
+    const humidityTxt = elementMaker('p', 'humidity-text');
+    const visibilityTxt = elementMaker('p', 'visibility-text');
+    const precipitationTxt = elementMaker('p', 'precipitation-text');
+    appendChildren(waterView, [pressureTxt, humidityTxt, visibilityTxt, precipitationTxt]);
 
-    const windView = elementMaker(div, 'wind-view'); //bottom right
-    const cloudinessTxt = elementMaker(p, 'cloud-text');
-    const windSpdTxt = elementMaker(p, 'wind-speed-text');
-    const windDirTxt = elementMaker(p, 'wind-direction-text');
+    const windView = elementMaker('div', 'wind-view'); //bottom right
+    const cloudinessTxt = elementMaker('p', 'cloud-text');
+    const windSpdTxt = elementMaker('p', 'wind-speed-text');
+    const windDirTxt = elementMaker('p', 'wind-direction-text');
     appendChildren(windView, [cloudinessTxt, windSpdTxt, windDirTxt])
-    
+
     appendChildren(infoView, [tempView, forecastView, waterView, windView]);
 
+    return {
+        topView,
+        midView,
+        infoView
+    }
+
 })
+
+export default view;
