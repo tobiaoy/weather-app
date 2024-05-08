26a3572bae5bb05f76512cdccab28444fe730bd5
Tobi Oyero <tobioyero@Tobis-MacBook-Pro-2.local>
2024-05-08 13:05:09 -0400

    refactored view

--- src/modules/view.js
+++ src/modules/view.js
@@ -1,6 +1,6 @@
 // set up the view of the page
 const appendChildren = (parent, ...children) => {
-    children.forEach(child => {
+    children.forEach((child) => {
         parent.appendChild(child);
     })
 }
@@ -17,68 +17,161 @@ const elementMaker = (el, id, type, txt, ph) => {
 
 const view = (() => {
     //top view -> input search bar | search btn |  celsius btn | fahrenheit btn
-    const topView = elementMaker('div', 'top-view');
+    const topView = document.createElement('div');
+    topView.setAttribute('id', 'top-view');
 
-    const searchBox = elementMaker('div', 'search-box');
-    const searchInput = elementMaker('input', 'search-input', 'text', '', 'search for a location');
-    const searchBtn = elementMaker('button', 'search-button', 'submit', 'Search');
-    appendChildren(searchBox, [searchInput, searchBtn]);
+    // search box 
+    const searchBox = document.createElement('div');
+    searchBox.setAttribute('id', 'search-box');
+    
+    const searchInput = document.createElement('input');
+    searchInput.setAttribute('id', 'search-input');
+    searchInput.setAttribute('type', 'text');
+    searchInput.setAttribute('placeholder', 'search for a location');
 
-    const sysBox = elementMaker('div', 'system-settings');
-    const celsiusBtn = elementMaker('button', 'celsius-button', txt='Celsius');
-    const fahrenheitBtn = elementMaker('button', 'fahrenheit-button', txt='Fahrenheit');
-    appendChildren(sysBox, [celsiusBtn, fahrenheitBtn]);
-    appendChildren(topView, [searchBox, sysBox]);
+    const searchBtn = document.createElement('button');
+    searchBtn.setAttribute('id', 'search-btn');
+    searchBtn.setAttribute('type', 'submit');
+    searchBtn.textContent = 'search'
+
+    appendChildren(searchBox, searchInput, searchBtn)
+    
+    
+    // system switch btns
+    const sysBox = document.createElement('div');
+    sysBox.setAttribute('id', 'sys-settings');
+
+    const celsiusBtn = document.createElement('button');
+    celsiusBtn.setAttribute('id', 'celsius-btn');
+    celsiusBtn.textContent = 'celsius';
+
+    const fahrBtn = document.createElement('button');
+    fahrBtn.setAttribute('id', 'fahr-btn');
+    fahrBtn.textContent = 'fahrenheit';
+
+    appendChildren(sysBox, celsiusBtn, fahrBtn)
+    
+    // adding main boxes to top view
+    appendChildren(topView, searchBox, sysBox)
+    
 
     //middle view -> location, time, date
-    const midView = elementMaker('div', 'mid-view');
-    const locationTxt = elementMaker('h4', 'location-text', '', '');
-    const descTxt = elementMaker('div', 'date-time');
-    const timeTxt = elementMaker('p', 'time-text','','');
-    const dateTxt = elementMaker('p', 'date-text','','');
-    appendChildren(descTxt, [timeTxt, dateTxt]);
-    appendChildren(midView, [locationTxt, descTxt]);
+    const midView = document.createElement('div');
+    midView.setAttribute('id', 'mid-view');
+
+    // location text
+    const locTxt = document.createElement('h4');
+    locTxt.setAttribute('id', 'loc-text');
+    
+    // date - time text
+    const dateTimeText = document.createElement('div');
+    dateTimeText.setAttribute('id', 'date-time-text');
+
+    const timeTxt = document.createElement('p');
+    timeTxt.classList.add('dtime-text');
+
+    const dateTxt = document.createElement('p');
+    dateTxt.classList.add('dtime-text');
+
+    appendChildren(dateTimeText, timeTxt, dateTxt);
+
+    // adding main box to mid view
+    appendChildren(midView, locTxt, dateTimeText);
     
 
     //info view -> temp, feels like temp, high/low | forecast (for the day)
     //             pressure, humidity, visibility | cloudiness, wind speed, wind direction
-    const infoView = elementMaker('div', 'info-view');
 
-    const tempView = elementMaker('div', 'tempView'); //top left
+    const infoView = document.createElement('div');
+    infoView.setAttribute('id', 'info-view');
 
-    const tempTxt = elementMaker('div', 'temp-text');
-    const actualTemp = elementMaker('p', 'actual-temp');
-    const feelsLikeTemp = elementMaker('p', 'feels-like-temp');
-    appendChildren(tempTxt, [actualTemp, feelsLikeTemp]);
+    // temp view -> top left
+    const tempView = document.createElement('div');
+    tempView.setAttribute('id', 'temp-view');
 
-    const tempDesc = elementMaker('div', 'temp-desc');
-    const tempIcon = elementMaker('img');
-    const tempSumm = elementMaker('p', 'temp-summary');
-    appendChildren(tempDesc, [tempIcon, tempSumm]);
+    // temp text
+    const tempTxt = document.createElement('div');
+    tempTxt.setAttribute('id', 'temp-text');
 
-    const highLow = elementMaker('div', 'high-low');
-    const todayHigh = elementMaker('p', 'todays-high');
-    const todayLow = elementMaker('p', 'todayLow');
-    appendChildren(highLow, [todayHigh, todayLow]);
+    const actualTemp = document.createElement('p');
+    actualTemp.setAttribute('id', 'actual-temp');
 
-    appendChildren(tempView, [tempTxt, tempDesc, highLow]);
+    const flTemp = document.createElement('p');
+    flTemp.setAttribute('id', 'fl-temp');
+    
+    appendChildren(tempTxt, actualTemp, flTemp);
 
-    const forecastView = elementMaker('div', 'forecast-view'); //top right -> to fill with forecast components later
+    // temp description
+    const tempDesc = document.createElement('div');
+    tempDesc.setAttribute('id', 'temp-desc');
 
-    const waterView = elementMaker('div', 'water-view'); //bottom left
-    const pressureTxt = elementMaker('p', 'pressure-text');
-    const humidityTxt = elementMaker('p', 'humidity-text');
-    const visibilityTxt = elementMaker('p', 'visibility-text');
-    const precipitationTxt = elementMaker('p', 'precipitation-text');
-    appendChildren(waterView, [pressureTxt, humidityTxt, visibilityTxt, precipitationTxt]);
+    const tempIcon = document.createElement('img');
+    
+    const tempSumm = document.createElement('p');
+    tempSumm.setAttribute('id', 'temp-summ');
 
-    const windView = elementMaker('div', 'wind-view'); //bottom right
-    const cloudinessTxt = elementMaker('p', 'cloud-text');
-    const windSpdTxt = elementMaker('p', 'wind-speed-text');
-    const windDirTxt = elementMaker('p', 'wind-direction-text');
-    appendChildren(windView, [cloudinessTxt, windSpdTxt, windDirTxt])
+    appendChildren(tempDesc, tempIcon, tempSumm);
 
-    appendChildren(infoView, [tempView, forecastView, waterView, windView]);
+    // high low
+    const highLow = document.createElement('div');
+    highLow.setAttribute('id', 'high-low'); 
+
+    const high = document.createElement('p');
+    high.classList.add('high-low');
+
+    const low = document.createElement('p');
+    low.classList.add('high-low');
+
+    appendChildren(highLow, high, low);
+
+    // add main boxes to temp view
+    appendChildren(tempView, tempTxt, tempDesc, highLow);
+
+
+    // forecast view -> top right
+    const forecastView = document.createElement('div');
+    forecastView.setAttribute('id', 'forecast-view');
+
+
+    // water view -> bottom left
+    const waterView = document.createElement('div');
+    waterView.setAttribute('id', 'water-view');
+
+    const pressureTxt = document.createElement('p');
+    pressureTxt.classList.add('water-text');
+
+    const humidTxt = document.createElement('p');
+    humidTxt.classList.add('water-text');
+
+    const visTxt = document.createElement('p');
+    visTxt.classList.add('water-text');
+
+    const precipTxt = document.createElement('p');
+    precipTxt.classList.add('water-text');
+
+    // add main boxes to water view
+    appendChildren(waterView, pressureTxt, humidTxt, visTxt, precipTxt);
+
+
+    // wind view -> bottom right
+    const windView = document.createElement('div');
+    windView.setAttribute('id', 'wind-view');
+
+    const cloudTxt = document.createElement('p');
+    cloudTxt.classList.add('wind-text');
+
+    const windSpdTxt = document.createElement('p');
+    windSpdTxt.classList.add('wind-text');
+
+    const windDirTxt = document.createElement('p');
+    windDirTxt.classList.add('wind-text');
+
+    // add main boxes to wind view
+    appendChildren(windView, cloudTxt, windSpdTxt, windDirTxt);
+
+    // add main boxes to info view
+    appendChildren(infoView, tempView, forecastView, waterView, windView);
+
 
     return {
         topView,
@@ -89,19 +182,29 @@ const view = (() => {
 })
 
 const createForecastBox = (hour, img, temp) => {
-    const box = elementMaker('div');
+    const box = document.createElement('div');
     box.classList.add('hour-forecast-box');
-    const displayHour = elementMaker('p', txt=`${hour}`);
-    const displayTemp = elementMaker('p', txt=`${temp}`);
-    const displayIcon = elementMaker('img');
-    displayIcon.setAttribute('src', img)
-    const tempBox = elementMaker('div')
+
+    // temp box -> for icon and forecast
+    const tempBox = document.createElement('div');
     tempBox.classList.add('forecast-temp-box');
-    appendChildren(tempBox, [displayIcon, displayTemp]);
-    appendChildren(box, [displayHour, tempBox]);
+
+    // display hr
+    const displayHour = document.createElement('p');
+    displayHour.textContent = `${hour}`;
+
+    // display temp
+    const displayTemp = document.createElement('p');
+    displayTemp.textContent = `${temp}`;
+
+    // display icon
+    const displayIcon = document.createElement('img');
+    displayIcon.setAttribute('src', img);
+
+    appendChildren(tempBox, displayIcon, displayTemp);
+    appendChildren(box, displayHour, tempBox);
 
     return box;
-
 }
 
 export {
